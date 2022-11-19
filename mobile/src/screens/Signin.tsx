eade30f7ab8ee9b5faa925624f1d93b832b807a7
Ricardo Machado <ricardo.machado10.rm@gmail.com>
2022-11-19 11:42:28 -0300

    finalizado o 1 dia do mobile

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Center, Text } from "native-base";
+
+export default function Signin() { 
+    return (
+        <Center flex={1} bgColor='black'>
+            <Text color='white' fontSize={24}>
+                Signin
+            </Text>
+        </Center>
+    )
+}
